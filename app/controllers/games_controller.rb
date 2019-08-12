4745e266f5e67f534404b78a3b98e25d9e3d45b6
olesya ogorodnikova <ooleszja@gmail.com>
2019-08-12 17:28:08 +0200

    rails new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,14 @@
 class GamesController < ApplicationController
   def new
-  end
+  @letters = ('A'..'Z').to_a.sample(10)
+
+end
 
   def score
+    @letters = ('A'..'Z').to_a.sample(10)
+    array = params[:answer].split
+    array.each_with_index do |word, index|
+    @letters.include?(word[index])
+    end
   end
 end
